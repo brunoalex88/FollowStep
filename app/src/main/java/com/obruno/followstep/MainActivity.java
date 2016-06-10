e91e879a1d447d4d967964368d4bd461a4e81346
obruno <brunoalex_88@yahoo.com.br>
2016-06-10 19:09:05 -0300

    Classe alterada para testes.

--- app/src/main/java/com/obruno/followstep/MainActivity.java
+++ app/src/main/java/com/obruno/followstep/MainActivity.java
@@ -1,13 +1,25 @@
 package com.obruno.followstep;
 
+import android.content.Intent;
+import android.location.Location;
+import android.support.annotation.IntegerRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.obruno.followstep.location.LocationClass;
+import com.obruno.followstep.service.LocationServiceClass;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String className = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, LocationServiceClass.class);
+        startService(intent);
     }
 }
